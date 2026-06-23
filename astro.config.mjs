c83d8d3f2e3ae632492f4ea56eedf604a8ac5f3e
shaunlottering <shaundlottering@gmail.com>
2026-06-23 17:09:01 +0200

    LDSchema + Compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     site: "https://connectedspeech.co.za",
+    compressHTML: true,
 
     devToolbar: {
         enabled: false,
