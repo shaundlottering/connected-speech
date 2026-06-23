d6bdc7a7da5a70e52fe4f3263669a05a8c16b50a
shaunlottering <shaundlottering@gmail.com>
2026-06-23 16:54:13 +0200

    Sitemap Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-      enabled: false,
-  },
+    site: "https://connectedspeech.co.za",
 
-  vite: {
-    plugins: [tailwindcss()],
-  },
+    devToolbar: {
+        enabled: false,
+    },
+
+    vite: {
+        plugins: [tailwindcss()],
+    },
+
+    integrations: [sitemap()],
 });
